96c310cdd3f136f13db2f4f5ca9bcbdc453b977c
youwol-guillaume <greinisch@youwol.com>
2023-08-21 12:32:07 +0200

    :bookmark: 0.2.0

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -119,7 +119,7 @@ const entries = {
 export const setup = {
     name:'@youwol/vsf-canvas',
         assetId:'QHlvdXdvbC92c2YtY2FudmFz',
-    version:'0.2.0-wip',
+    version:'0.2.0',
     shortDescription:"3D rendering of vs-flow's workflow.",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/vsf-canvas&tab=doc',
     npmPackage:'https://www.npmjs.com/package/@youwol/vsf-canvas',
